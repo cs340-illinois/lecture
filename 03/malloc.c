963a497c0b705d10494cbe52c1534a22ebf3bb2c
Wade Fagen-Ulmschneider <waf@illinois.edu>
2023-01-23 15:46:06 -0600

    03 update

--- 03/malloc.c
+++ 03/malloc.c
@@ -1,10 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef struct _myObject {
+  int value;
+  char *s;
+} myObject;
+
 int main() {
   char *s = malloc(10);
-  int *num = malloc( sizeof(int) );
+  myObject *obj = malloc( sizeof(myObject) );
+  obj->value = 3;
   
-  printf("%p %p\n", s, num);
+  printf("%p %p %d\n", s, obj, obj->value);
   return 0;
 }
