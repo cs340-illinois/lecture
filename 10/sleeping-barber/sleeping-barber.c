8c0d061053410e4ed1b7bed7462b94325187ef2a
Wade Fagen-Ulmschneider <waf@illinois.edu>
2023-02-15 19:45:10 -0600

    10

---
+++ 10/sleeping-barber/sleeping-barber.c
@@ -0,0 +1,53 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <pthread.h>
+#include "sleeping-barber.h"
+
+struct _sleeping_barber_t {
+
+
+
+};
+typedef struct _sleeping_barber_t sleeping_barber_t;
+
+void init(sleeping_barber_t *sb) {
+
+
+}
+
+void barber(sleeping_barber_t *sb) {
+  while (1) {  
+
+
+
+
+
+
+
+
+
+    cut_hair("waf");
+
+
+
+
+
+
+
+  }
+}
+
+void customer(sleeping_barber_t *sb, const char *name) {
+  // Each customer is a thread! :)
+
+
+
+
+
+
+
+
+
+
+  
+}
